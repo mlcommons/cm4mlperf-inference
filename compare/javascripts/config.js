3e13926e0a70419cb5558dd9057412aaaab485e5
arjunsuresh <arjunsuresh@users.noreply.github.com>
2024-11-19 13:12:52 +0000

    Deployed 691385f with MkDocs version: 1.6.1

---
+++ compare/javascripts/config.js
@@ -0,0 +1,7 @@
+const results_version="v4.1";
+var repo_owner="mlcommons";
+var repo_branch="mlperf-inference-results-scc24";
+var repo_name="cm4mlperf-inference";
+const dbVersion ="136";
+const default_category ="datacenter";
+const default_division ="open";
